ee0dab3a3a3d1c5558b97a3c46776498a37d6c0e
Van Huynh <vthuynh@cpp.edu>
2022-01-23 11:56:13 -0800

    add scss styles and fix initial structure of homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './homepage.styles.scss';
+
 const HomePage = () => (
     <div className="homepageq">
         <div className="directory-menu">
@@ -9,32 +11,24 @@ const HomePage = () => (
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">JACKETS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">SNEAKERS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">WOMEN</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">MEN</h1>
