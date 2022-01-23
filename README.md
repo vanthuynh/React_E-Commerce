9899aa03d5fa20bc7e1a15e9a5554223404d1add
Van Huynh <vthuynh@cpp.edu>
2022-01-23 13:09:35 -0800

    breaking out homepage and directory/menu-item components

---
+++ README.md
@@ -0,0 +1,7 @@
+# E-Commerce Project Site built with React
+## (based on knowledge learnt from Andrei's ReactDev course)
+
+### Diary
+- create-react-app does bring minor problems with Eslint package, fixed by:
+ + installing eslint@7.32.0
+ + completely remove node_modules folder and reinstall it
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './menu-item.styles.scss';
+
+// destructuring title is just like pass props as parameter and do props.title
+const MenuItem = ({ title }) => (
+    <div className="menu-item">
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
