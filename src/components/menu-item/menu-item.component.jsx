3339956d80722fd9ac02984ffc6fdb68a1aea62c
Van Huynh <vthuynh@cpp.edu>
2022-01-23 13:37:15 -0800

    correct background image size and adjust hover effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,13 +4,14 @@ import './menu-item.styles.scss';
 
 // destructuring title is just like pass props as parameter and do props.title
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div 
-        style={{ // add style property that can add style directly
-            // this is Javascript template string
-            backgroundImage: `url(${imageUrl})`
-        }} 
-        className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+        <div 
+            className='background-image' 
+            style={{ // add style property that can add style directly
+                // this is Javascript template string
+                backgroundImage: `url(${imageUrl})`
+            }}  
+        />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
