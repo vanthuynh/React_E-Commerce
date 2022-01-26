27833512fcfba9af7081cc023375cb4866ce80a8
Van Huynh <vthuynh@cpp.edu>
2022-01-25 23:22:39 -0800

    add better styling for custombutton and SignOut feature

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,8 +4,8 @@ import './custom-button.styles.scss'
 
 // stateless functional component
 // button can take the property of type 'submit' and trigger the onSubmit method
-const CustomButton = ({ children, ...otherProps }) => (
-    <button className='custom-button'{...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`}{...otherProps}>
         {children}
     </button>
 )
